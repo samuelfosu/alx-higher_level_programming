acb3da5d06b465da5c09335b307c09e8ea4b3b7a
samuelfosu <sfosu003@gmail.com>
2023-03-13 11:07:18 -0700

    Add Task 1. Secure access to an element in a list file

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
