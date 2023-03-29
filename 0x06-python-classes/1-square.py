de9510e37ce7c26cba9b21185e98d5773a200373
samuelfosu <sfosu003@gmail.com>
2023-03-29 10:37:17 -0700

    Update 0x06. Python - Classes and Objects files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
+"""Class Square that defines a square"""
+
+
 class Square:
-    """Class Square that defines a square object
-    """
+    """Private instance attribute: size"""
     def __init__(self, size):
-        """Initialize method that stores the size of the square
-        Args:
-            param1 (int): size of the square
-        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
+"""Class Square that defines a square"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Private instance attribute: size"""
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
+        """Instantiation with optional size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = int(size)
+            self.__size = size
