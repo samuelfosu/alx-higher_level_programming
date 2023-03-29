8b387410ddc0882d0354e38abe566c112ab48640
samuelfosu <sfosu003@gmail.com>
2023-03-29 11:28:40 -0700

    Update Task 1. Safe printing of an integers list file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,15 +3,13 @@
 
 def safe_print_integer(value):
     """ Prints an integer with "{:d}".format().
-
     Args:
     value - value to be printed
-
     Returns:
     True if value has been correctly printed as integer
     """
     try:
         print("{:d}".format(value))
-        return {True}
+        return (True)
     except (ValueError, TypeError):
         return (False)
