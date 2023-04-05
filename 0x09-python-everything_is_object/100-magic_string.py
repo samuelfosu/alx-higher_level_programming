d1acf22ad8f1a67f42bd197b9dc3d76d8d1dffd7
samuelfosu <sfosu003@gmail.com>
2023-04-04 18:27:48 -0700

    Update Task 19. Copy a list object file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.num = getattr(magic_string, 'num', 0) + 1
-    return ("BestSchool, " * (magic_string.num - 1) + "BestSchool")
-Footer
+def magic_string(l=[]):
+    l += ["BestSchool"]
+    return ", ".join(l)
