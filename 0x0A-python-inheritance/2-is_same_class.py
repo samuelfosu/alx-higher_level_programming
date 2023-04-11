81ee971a88eed043cafec30de1cfadac015ba080
samuelfosu <sfosu003@gmail.com>
2023-04-11 13:00:49 -0700

    Add Task 2. Exact same object file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True/False if obj is a type of a_class"""
+    return type(obj) == a_class
