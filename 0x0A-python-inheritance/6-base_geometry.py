049604747ac930fa77f23e13451d426c603b1f5a
samuelfosu <sfosu003@gmail.com>
2023-04-11 13:30:57 -0700

    Add Task 6. Improve Geometry file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """Empty class"""
+        raise Exception("area() is not implemented")
