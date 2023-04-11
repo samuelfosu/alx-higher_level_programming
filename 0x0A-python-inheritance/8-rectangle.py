6a34423711c96b3e55563ac1bdfbda99fc57a9ea
samuelfosu <sfosu003@gmail.com>
2023-04-11 13:43:45 -0700

    Update Task 8. Rectangle file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Rectangle class module"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle from BaseGeometry"""
+    """ Class that defines a rectangle from BaseGeometry Class """
+
     def __init__(self, width, height):
-        """Instantiation width and height
-        as private attributes"""
-        self.__width = width
-        self.__height = height
+        """ Initializes instance """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height 
