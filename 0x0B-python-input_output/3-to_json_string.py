4253a42964d500bb8ce1831a30074204a58e6648
samuelfosu <sfosu003@gmail.com>
2023-04-12 19:54:38 -0700

    Add Task 3. To JSON string file

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that returns the JSON representation of an object"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """A function that returns the JSON representation of an object"""
+    return json.dumps(my_obj)
