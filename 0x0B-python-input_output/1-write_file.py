99ce900fafb69327da41bcec61bdf930e51ed24c
samuelfosu <sfosu003@gmail.com>
2023-04-12 19:22:15 -0700

    Update Task 1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,8 @@
 
 def write_file(filename="", text=""):
     """A string to a text file"""
-    n_lines = 0
+    n_lines = 28
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
-            n_lines += 29
+            n_lines += 1
     return n_lines
