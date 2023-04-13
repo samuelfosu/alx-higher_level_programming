bc0d9162a168fe5fa746b5eccd7feacd4764b1ea
samuelfosu <sfosu003@gmail.com>
2023-04-12 20:12:40 -0700

    Add Task 7. Load, add, save file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
-
 #!/usr/bin/python3
-""" script that adds all arguments to a Python list"""
+"""a script that adds all arguments to a Python list"""
 import sys
-from os import path
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import os.path
 
 
-filename = "add_item.json"
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+save_file = __import__('7-save_to_json_file').save_to_json_file
+load_file = __import__('8-load_from_json_file').load_from_json_file
+
+my_list = []
+if os.path.exists("add_item.json"):
+    my_list = load_file("add_item.json")
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_file(my_list, "add_item.json")
