a79370a44605c9381dc8a641059f9c7c62eb8ce8
samuelfosu <sfosu003@gmail.com>
2023-04-12 19:19:45 -0700

    Add Task 1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """A string to a text file"""
+    n_lines = 0
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            n_lines += 29
+    return n_lines
