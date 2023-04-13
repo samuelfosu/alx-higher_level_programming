d30b889e4f16dd55470582fac1b42bd0f21254b0
samuelfosu <sfosu003@gmail.com>
2023-04-12 20:41:46 -0700

    Add Task 12. Pascal's Triangle file

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a function def pascal_triangle(n"""
+
+
+def pascal_triangle(n):
+    """Returns integers
+    representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        triangle.append([1])
+        for j in range(1, i):
+            triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle[i].append(1)
+    return triangle
