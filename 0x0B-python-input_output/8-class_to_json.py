9f699b08d9708628f643785698ad92f2a21262e8
samuelfosu <sfosu003@gmail.com>
2023-04-12 20:29:04 -0700

    Add TAsk 8. Class to JSON file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a function that returns the dictionary description"""
+
+
+def class_to_json(obj):
+    """a function that returns the dictionary description"""
+    return obj.__dict__
