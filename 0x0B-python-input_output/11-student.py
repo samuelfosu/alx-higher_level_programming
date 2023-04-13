328584c8cef062c90f810941ebbe3465193afac0
samuelfosu <sfosu003@gmail.com>
2023-04-12 20:39:32 -0700

    Add Task 11 Student to disk and reload file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,7 +3,7 @@
 
 
 class Student:
-    """Defines by: first_name, last_name and age"""
+    """Defines a student"""
     def __init__(self, first_name, last_name, age):
         """Instantiation"""
         self.first_name = first_name
@@ -16,3 +16,8 @@ class Student:
             return self.__dict__
         else:
             return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes"""
+        for k, v in json.items():
+            self.__dict__[k] = v
