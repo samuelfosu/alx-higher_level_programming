c837e513a2e3158c5b6750e15ca4c0de33cdc2ff
samuelfosu <sfosu003@gmail.com>
2023-04-13 18:31:40 -0700

    Add Task 1. My list file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """a class MyList that inherits from list"""
+    def print_sorted(self):
+        print(sorted(self))
