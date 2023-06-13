40b9d6fc9cfc61264f6df9441c3a28e51261a82d
samuelfosu <sfosu003@gmail.com>
2023-06-12 19:34:07 -0700

    Update Project 0x12. JavaScript - Warm up files

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = process.argv.slice(2);
 if (isNaN(x[0])) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < x[0]; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x[0]; i++) {
+    console.log('C is fun');
+  }
 }
