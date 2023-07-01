8511458a410d9b7d58db8fbb44658b8a5c504909
Samuel Fosu <sfosu003@gmail.com>
2023-06-30 20:58:07 -0700

    Add Task 3 Error code #0 file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that sends request to URL and displays response (decoded in utf-8)."""
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as reponse:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}' .format(err.code))
