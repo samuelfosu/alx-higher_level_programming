1936111c6ee81d863ef4665a83df23fa1f5391e7
Samuel Fosu <sfosu003@gmail.com>
2023-06-30 21:54:11 -0700

    Add Task Error code #1 file

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Script sends URL and displays the body"""
-import requests
-import sys
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}". format(r.status_code))
+    import requests
+    import sys
+
+    res = requests.get(sys.argv[1])
+
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
     else:
-        print(r.text)
+        print(res.text)
