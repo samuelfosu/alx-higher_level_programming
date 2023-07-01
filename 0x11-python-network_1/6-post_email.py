aee760d7626c2c24b0b05f44715bc1da83ee1137
Samuel Fosu <sfosu003@gmail.com>
2023-06-30 21:20:15 -0700

    Add Task 6 POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script sends post request to passed URL with the email as a parameter"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
+    except:
+        pass
