32afdeaa090afbb3dec1233c8c8081f330f29398
Samuel Fosu <sfosu003@gmail.com>
2023-06-30 20:38:19 -0700

    Add Task 1. Response header value #0 file

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Script sends URL, request URL and display Request-Id variable found"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get('X-Request-Id'))
