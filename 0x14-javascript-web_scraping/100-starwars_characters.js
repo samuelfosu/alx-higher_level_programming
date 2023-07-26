068632cc7ebcbee83e61a3161bbcf96394c64acb
Samuel Fosu <sfosu003@gmail.com>
2023-07-25 19:45:40 -0700

    Add tAsk 7. Who was playing in this movie? file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const req = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
