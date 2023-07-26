b4a234c3813baa4a33aee48357513de7b0f2dd09
Samuel Fosu <sfosu003@gmail.com>
2023-07-25 19:30:39 -0700

    Add Task 6. How many completed? file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (err, response, body) {
+if (!process.argv || process.argv.length < 3) {
+  console.log('Missing arguments');
+  process.exit(1);
+}
+request(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
+  } else {
+    const results = JSON.parse(body);
     const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].completed) {
+        if (completed[results[i].userId]) {
+          completed[results[i].userId]++;
         } else {
-          completed[task.userId]++;
+          completed[results[i].userId] = 1;
         }
       }
     }
     console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
-});
+}
+);
